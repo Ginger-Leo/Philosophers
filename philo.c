0eaf008ec81acc184b50e6304cf7bbba33067c10
Felipe Dessoy Caraballo <fdessoycaraballo@gmail.com>
2024-06-20 11:45:29 +0300

    fixed this shit yall

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: fdessoy- <fdessoy-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 12:26:25 by fdessoy-          #+#    #+#             */
-/*   Updated: 2024/06/20 11:35:42 by fdessoy-         ###   ########.fr       */
+/*   Updated: 2024/06/20 11:44:41 by fdessoy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	philosophize(t_data **data, t_overseer *overseer)
 	overseer->start_time = what_time_is_it();
 	while (i < overseer->no_of_philosophers)
 	{
-		if (pthread_create(&data[i]->p_thread, NULL, &dinner_for_x, &data[i]) != 0)
+		if (pthread_create(&data[i]->p_thread, NULL, &dinner_for_x, data[i]) != 0)
 			nuka_cola("Thread creation failed\n", overseer, data[i]);
 		i++;
 	}
@@ -35,30 +35,28 @@ void	philosophize(t_data **data, t_overseer *overseer)
 
 void	*dinner_for_x(void *data)
 {
-	t_data			**p_data;
-	t_overseer		*o_data;
+	t_data			*p_data;
 	int				i;
 
-	p_data = (t_data **)data;
-	o_data = (*p_data)->overseer;
+	p_data = (t_data *)data;
 	i = 0;
-	if ((*p_data)->philo_id % 2 == 0)
+	if (p_data->philo_id % 2 == 0)
 		ft_usleep(20);
-	while (o_data->death_flag == 0) //|| o_data->eaten_flag != 1
+	while (p_data->overseer->death_flag == 0) //|| o_data->eaten_flag != 1
 	{
-		if (eating(p_data[i], o_data) == 0)
+		if (eating(p_data, p_data->overseer) == 0)
 			break ;
-		if (sleeping(p_data[i], o_data) == 0)
+		if (sleeping(p_data, p_data->overseer) == 0)
 			break ;
-		if (thinking(p_data[i], o_data) == 0)
+		if (thinking(p_data, p_data->overseer) == 0)
 			break ;
-		if (dying(p_data[i], o_data) == 0)
+		if (dying(p_data, p_data->overseer) == 0)
 			break ;
-		if (i == o_data->no_of_philosophers - 1)
+		if (i == p_data->overseer->no_of_philosophers - 1)
 			i = 0;
 		else
 			i++;
 	}
-	nuka_cola(NULL, o_data, p_data[i]);
+	nuka_cola(NULL, p_data->overseer, p_data);
 	return (data);
 }
